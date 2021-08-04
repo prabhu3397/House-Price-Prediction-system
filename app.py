40e41351c9ebefe7c35a081bcae15bf21cfa92f3
prabhu3397 <74896218+prabhu3397@users.noreply.github.com>
2021-08-04 23:33:45 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from os import defpath
+from flask import Flask, render_template,request,jsonify
+import pickle
+
+from flask.json import load
+import numpy as np
+
+
+app = Flask(__name__)
+model = pickle.load(open('best_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    int_features = [float(x) for x in request.form.values()]
+    final_features = np.array(int_features).reshape((1,-1))
+    prediction = model.predict(final_features)
+
+    output =prediction
+
+    return render_template('index.html', prediction_text='House price is $ {}'.format(output))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
